f32e887c14f8c055987d83bda7942f5177c27c53
Meshal1212222 <Meshal.hgz@gmail.com>
2025-12-23 18:00:05 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// الصفحة الرئيسية
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'home.html'));
+});
+
+// التقرير الشامل
+app.get('/report', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// الرسم البياني الشامل
+app.get('/comprehensive', (req, res) => {
+    res.sendFile(path.join(__dirname, 'comprehensive.html'));
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log('Server running on port ' + PORT);
+});
